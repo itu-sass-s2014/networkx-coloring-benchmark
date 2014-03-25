b2ce861b3c565aa2eefc3752e8efb30813749d8a
Henrik Haugbølle <henrikhaugboelle@gmail.com>
2014-03-25 14:31:25 +0100

    added hc and shc and rewrote benchmark to handle these

--- benchmark.py
+++ benchmark.py
@@ -1,7 +1,15 @@
 import networkx as nx
+import sys
 import re
 import glob
 import time
+import argparse
+
+parser = argparse.ArgumentParser(description='Run benchmark on networkx graph coloring algorithms')
+parser.add_argument('--folder', default=".", required=True, help="choose which folder to point to with .col files")
+parser.add_argument('--output', default=".", required=False, help="choose which folder to output .csv file to")
+
+args = parser.parse_args()
 
 current_milli_time = lambda: int(round(time.time() * 1000))
 
@@ -22,14 +30,18 @@ def getGraphFromFile(filename):
 
 information = dict()
 
-with open('./instances/information.csv', 'r') as lines:
-	for line in lines:
-		parts = line.replace('\n', '').split(',')
-		information[parts[0]] = parts
+try:
+	with open(args.folder + '/information.csv', 'r') as lines:
+		for line in lines:
+			parts = line.replace('\n', '').split(',')
+			information[parts[0]] = parts
+except IOError as e:
+    print "running without supplementary information.csv file"
 
-with open('./output.csv', 'w') as output:
-	for filename in glob.glob("./instances/*.col"):
+with open(args.output + '/output.csv', 'w') as output:
+	for filename in glob.glob(args.folder + '/*.col'):
 		key = filename.split('/').pop()
+		print key
 		G = getGraphFromFile(filename)
 
 		strategies = ['lf', 'sf', 'sl', 'gis', 'cs-bfs', 'cs-dfs']
@@ -38,36 +50,50 @@ with open('./output.csv', 'w') as output:
 			times = []
 			times_ic = []
 
+			print key, strategy
+
 			for i in range(0, 5):
 				start_time = current_milli_time()
 				result = nx.coloring(G, strategy=strategy, interchange=False, returntype='sets')
 				times.append(current_milli_time() - start_time)
 
+			print key, strategy, "interchange"
+
 			for i in range(0, 5):
 				start_time = current_milli_time()
-				result_ic = nx.coloring(G, strategy=strategy, interchange=False, returntype='sets')
+				result_ic = nx.coloring(G, strategy=strategy, interchange=True, returntype='sets')
 				times_ic.append(current_milli_time() - start_time)
 
 			average_time = sum(times) / len(times)
 			average_time_ic = sum(times_ic) / len(times_ic)
-
-			info = information[key]
-			if info[3] == '?':
-				info[3] = -1
-			
-			instance_optimal = int(info[3])
-			instance_nodes = info[1]
-			instance_edges = info[2]
 
 			result_optimal = len(result)
 			result_optimal_ic = len(result_ic)
 			result_nodes = G.number_of_nodes()
 			result_edges = G.number_of_edges()
 
-			diff_percent = 100 * (result_optimal - instance_optimal) / instance_optimal
-			diff_number = result_optimal - instance_optimal
-			diff_percent_ic = 100 * (result_optimal_ic - instance_optimal) / instance_optimal
-			diff_number_ic = result_optimal_ic - instance_optimal
+			if key in information:
+				info = information[key]
+				if info[3] == '?':
+					info[3] = -1
+				
+				instance_optimal = int(info[3])
+				instance_nodes = info[1]
+				instance_edges = info[2]
+
+				diff_percent = 100 * (result_optimal - instance_optimal) / instance_optimal
+				diff_number = result_optimal - instance_optimal
+				diff_percent_ic = 100 * (result_optimal_ic - instance_optimal) / instance_optimal
+				diff_number_ic = result_optimal_ic - instance_optimal
+			else:
+				instance_optimal = -1
+				instance_nodes = -1
+				instance_edges = -1
+
+				diff_percent = -1
+				diff_number = -1
+				diff_percent_ic = -1
+				diff_number_ic = -1
 			
 			output.write(str.format("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13},{14},{15},{16}\n", key, strategy, instance_nodes, result_nodes, int(instance_nodes)==result_nodes, instance_edges, result_edges, int(instance_edges)==result_edges, instance_optimal, result_optimal, diff_number, diff_percent, average_time, result_optimal_ic, diff_number_ic, diff_percent_ic, average_time_ic))
 			
